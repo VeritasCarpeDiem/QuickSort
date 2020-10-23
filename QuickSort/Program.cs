fecc3fd590e815df6f69bde8877f96b63daecb96
GMR Student <gmrstudent@outlook.com>
2020-10-23 15:53:16 -0700

    finished hoare and lomuto

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace QuickSort
 {
     class Program
     {
+        static bool isArrSorted(int[] arr)
+        {
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if (arr[i] > arr[i + 1] )
+                {
+                    ;
+                    return false;
+                }
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 5, 4, 3, 6, 2, 1, 7 };
+
+            int[] lomuto = new int[] { 7, 4, 3, 6, 2, 1, 5 };
+
+            Console.WriteLine(isArrSorted(arr));
+
+            QuickSort.LomutoQuickSort(lomuto);
+
+            Console.WriteLine(isArrSorted(arr));
         }
     }
 }
--- QuickSort/QuickSort.cs
+++ QuickSort/QuickSort.cs
@@ -4,33 +4,93 @@ using System.Text;
 
 namespace QuickSort
 {
-    class QuickSort
+    class QuickSort //where T:IComparable<T>
     {
         private static int LomutoPartition(int[] arr,int low, int high)
         {
             int pivot = arr[high]; //set pivot to highest value of arr
             int wall = low - 1; 
 
-            for (int i = 0; i < arr.Length; i++)
+            for ( int i = low; i < high; i++) //can't be arr.Length
             {
-                if (arr[i]<pivot)
+                if (arr[i] < pivot)
                 {
-                    Swap(ref arr[i], ref arr[wall]);
                     wall++;
+                    Swap(ref arr[i], ref arr[wall]);
+                    
                 }
-                //ignore any values are greater than pivot
+                //ignore any values that are greater than pivot
             }
 
-            Swap(ref arr[pivot],ref arr[wall+1]);
+            //guarentees pivot is in the right spot so we can exclude
+            //it in next recursive calls
+            Swap(ref arr[high],ref arr[wall+1]);
             return wall + 1;
         }
-        public static int[] QuickSortLomuto(int arr)
+        public static int[] LomutoQuickSort(int[] arr) //the return type can void b/c arrays passed bvy reference
+        {
+            QuickSort(arr, 0, arr.Length-1);
+
+            return arr;
+
+            void QuickSort(int[] arr, int low, int high) //helper function 
+            {
+                if (low < high)
+                {
+                    int pivot = LomutoPartition(arr, low, high);
+
+                    ;
+                    QuickSort(arr, low, pivot-1);//sort left
+
+                    QuickSort(arr, pivot + 1, high); //sort right
+                }
+            }
+        }
+        public static int[] HoareQuickSort(int[] arr)
         {
             
-        }
-        void HoarePartition()
-        {
+            QuickSort(arr, 0, arr.Length - 1);
+            
+            return arr;
 
+            void QuickSort(int[] arr,int low, int high) //helper function 
+            { 
+                if (low < high)
+                {
+                    int pivot = HoarePartition(arr, low, high);
+
+                    ;
+                    QuickSort(arr, low, pivot);//sort left
+
+                    QuickSort(arr, pivot + 1, high); //sort right
+                }
+            }
+            
+        }
+        private static int HoarePartition(int[] arr, int low, int high)
+        {
+            int pivot = arr[low];
+            int leftPtr = low - 1;
+            int rightPtr = high + 1;
+
+            while(true)
+            {
+                do
+                {
+                    leftPtr++;
+                } while (arr[leftPtr].CompareTo(pivot) < 0);
+                do
+                {
+                    rightPtr--;
+                } while (arr[rightPtr].CompareTo(pivot) > 0);
+               
+                if(leftPtr>=rightPtr) //?
+                {
+                    return rightPtr; //this is new "wall"
+                 
+                }
+                Swap(ref arr[leftPtr], ref arr[rightPtr]);
+            }
         }
         static void Swap(ref int num1, ref int num2)
         {
